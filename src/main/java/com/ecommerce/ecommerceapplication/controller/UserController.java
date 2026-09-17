d2f5b06b79d47e17067c9c2028df7411156323f2
RITIK CHAUHAN <ritikchauhan63@gmail.com>
2026-09-17 22:40:17 +0530

    controller layer

--- src/main/java/com/ecommerce/ecommerceapplication/controller/UserController.java
+++ src/main/java/com/ecommerce/ecommerceapplication/controller/UserController.java
@@ -1,38 +1,41 @@
-// package com.ecommerce.ecommerceapplication.controller;
+package com.ecommerce.ecommerceapplication.controller;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.ecommerce.ecommerceapplication.dto.RegisterRequest;
-// import com.ecommerce.ecommerceapplication.model.User;
-// import com.ecommerce.ecommerceapplication.service.UserService;
-// import com.ecommerce.ecommerceapplication.entity.User;
+import com.ecommerce.ecommerceapplication.dto.RegisterRequest;
+import com.ecommerce.ecommerceapplication.entity.User;
+import com.ecommerce.ecommerceapplication.service.UserService;
 
-// @RestController
-// @RequestMapping("/api/users")
-// public class UserController {
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
 
-//     private final UserService userService;
+    private final UserService userService;
 
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-//     @PostMapping("/register")
-//     public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
-//         User user = userService.registerUser(
-//             request.getName(),
-//             request.getEmail(),
-//             request.getPassword()
-//         );
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        User user = userService.registerUser(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword());
 
-//         return ResponseEntity.status(HttpStatus.CREATED).body(user);
-//     }
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
-//     @GetMapping("/{id}")
-//     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-//         User user = userService.getUserById(id);
-//         return ResponseEntity.ok(user);
-//     }
-// }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+}
